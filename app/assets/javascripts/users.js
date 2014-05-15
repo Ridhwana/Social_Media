6cd5a56cbee1e51ce414c5c93ff8a8c246c3ccda
Ridhwana <Ridhwana.khan16@gmail.com>
2014-05-15 02:54:53 +0200

    update the modal to sync with the summary

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -12,6 +12,10 @@ function activate_modal(){
 //     this.email = ko.observable(data.email);
 // }
 
+function updateWorkingHistorySummary() {
+$('#workingHistorySummary').text($('#workingHistoryModal').val()).change()
+}
+
 function UserViewModel() {
     // Data
     $.getJSON("/api/users/1", function(allData) {
